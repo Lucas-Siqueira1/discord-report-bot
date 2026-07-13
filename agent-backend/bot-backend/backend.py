20beb14daf14435ef9435ac7c44263183155eca3
Lucas Siqueira <lucashenriquesiqueira7@gmail.com>
2026-07-13 16:13:42 -0300

    Estruturando monorepo: separando agent-backend e bot-backend como serviços independentes

--- agent-backend/bot-backend/backend.py
+++ agent-backend/bot-backend/backend.py
@@ -1,3 +1,26 @@
+<<<<<<< HEAD
+from fastapi import FastAPI
+from pydantic import BaseModel, TypeAdapter
+from typing import List
+from main import run
+
+app = FastAPI()
+
+class Mensagem(BaseModel):
+    autor: str
+    hora: str
+    texto: str
+
+class ListaMensagens(BaseModel):
+    mensagens: List[Mensagem]
+
+@app.post("/report")
+async def gerar_report(mensagens_obj: ListaMensagens):
+
+    mensagens_str = mensagens_obj.model_dump_json()
+    report = await run(mensagens_str)
+    return report
+=======
 import discord
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
@@ -69,3 +92,4 @@ async def rotina_semanal():
     
 bot.run(token)
     
+>>>>>>> temp-bot/main
